29a5029046f71f6ba2f909057196eec365dc706f
shreya18469 <84868815+shreya18469@users.noreply.github.com>
2023-04-15 23:42:12 +0000

    Finished Challenge 1

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import base64
 import binascii
 
+
 def hex_to_base64(hex_str):
   #hex_bytes = bytes.fromhex(hex_str)
   #base64_bytes = base64.b64encode(hex_bytes)
@@ -10,5 +11,9 @@ def hex_to_base64(hex_str):
   str_base64 = base64.b64encode(str_bytes)
   return str_base64
 
+
 #test case works!
-print(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
+print(
+  hex_to_base64(
+    "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+  ))
