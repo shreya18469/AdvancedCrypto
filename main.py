7a30cc19f1a8c55daf9e5426447c73e497fb9434
shreya18469 <84868815+shreya18469@users.noreply.github.com>
2023-04-15 23:28:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#hex to base64 conversion
+import base64
+import binascii
+
+def hex_to_base64(hex_str):
+  #hex_bytes = bytes.fromhex(hex_str)
+  #base64_bytes = base64.b64encode(hex_bytes)
+  #base64_str = base64_bytes.decode('utf-8')
+  str_bytes = binascii.unhexlify(hex_str)
+  str_base64 = base64.b64encode(str_bytes)
+  return str_base64
+
+#test case works!
+print(hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
