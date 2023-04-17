4280d9d6c920825d1d0e055b3b3df11e2838a1be
shreya18469 <84868815+shreya18469@users.noreply.github.com>
2023-04-17 00:40:55 +0000

    Finished Challenge 1

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-#hex to base64 conversion
 import base64
 import binascii
+#Challenge 1- convert Hex to base64
+def hexToBytes(hex_str):
+  return bytes.fromhex(hex_str)
+
+def base64ToBytes(base64_str):
+  return base64.b64decode(base64_str)
+
+def bytesToHex(byte_str):
+  return byte_str.hex()
+
+def bytesToBase64(hex_str):
+  return base64.b64encode(byte_str).decode('utf-8')
+
+hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+byte_str = hexToBytes(hex_str)
+base64_str = bytesToBase64(byte_str)
+print(base64_str)
 
 
-def hex_to_base64(hex_str):
-  #hex_bytes = bytes.fromhex(hex_str)
-  #base64_bytes = base64.b64encode(hex_bytes)
-  #base64_str = base64_bytes.decode('utf-8')
-  str_bytes = binascii.unhexlify(hex_str)
-  str_base64 = base64.b64encode(str_bytes)
-  return str_base64
 
 
-#test case works!
-print(
-  hex_to_base64(
-    "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-  ))
+
